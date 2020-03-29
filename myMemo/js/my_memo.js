8a4a9690bd0f5318571c0012b53ec59a6fe40cc7
各⾃の登録名 <mtmt.703@gmail.com>
2020-03-29 17:41:40 +0900

    upload

--- myMemo/js/my_memo.js
+++ myMemo/js/my_memo.js
@@ -1,31 +1,24 @@
 // グローバル変数はここに書くほうが◎ $(function()の中じゃなくてOK
+// 新規用　整数だと編集されてしまうから　1を入れたらそこの場所が編集されたい何もされない判断として−1を自分で定義した
 var listNum = -1;
+
 var arr =[];
 
 $(function(){
     loadList();
 
-    // .titleにフォーカスがあたったら　イベントを書く
-        // フォーカスがあたっている.titleが空白であれば新規
-        // listNum = -1;
-
-        // それ以外は編集
-        // listNum = フォーカスがあたっているリストの番号
-    
         // 「documentの中の'.task-list li .title'にfocusされたら」という意味
         $(document).on('focus', '.task-list li .title', function () {
-            // テキスト取得
-            var value = $(this).text();
-    
-            if(value){
-                console.log(value);
-            } else {
-                console.log('から');
-            }
-    
+            
+            // タップされたリストの番号を取得
+            // クリックしている<li>が何番目か取得して、listNumに入れたい
+            var liTag =$(this).parent();
+
+            // listNumに現在のリスト番号を保存
+            listNum = $('.task-list li').index(liTag);
+            console.log(listNum);
         });  
         
-
 
     // エンターキーが押されたらテキストを保存する
     $('.task-list').keypress('.title',function(key){
@@ -36,32 +29,41 @@ $(function(){
             // console.log('エンター');
             console.log($(':focus'));
 
-            // ★listNumを取得★
-            
+            $(window).scrollTop($(window).scrollTop());
 
             // 配列にタイトルを入れる
             // var titleStr = $(this).text();
             var titleStr = $(':focus').text();
 
-            // もしlistNumが−1だったら新規用のコードを書く
-            // それ以外は編集用にコードを書く
+            // 配列にタイトルを入れる
+            // ラジオボタンがチェックされているかどうか
+            var checkStr = $(':focus').hasClass('checked');
+            console.log(checkStr);
             
+            // 新規＆編集用のコード (新規という概念をなくす)
+            // false=「チェックされていないのがfalse」というルールをこちらで決めている
+            // var checkStr = false;
+            // var memoObj = {
+            //     title:titleStr,
+            //     check:checkStr     
+            // }
+            // var checkStr = false;
 
-            // 新規用のコード 
-            // false=「データがない」という意味
-            var checkStr = false;
             var memoObj = {
                 title:titleStr,
                 check:checkStr     
             }
-
-            arr.push(memoObj);
+            // listNumの番号の配列に保存
+            arr[listNum] = memoObj;
             
             // localStorageに保存
             var arrStr =JSON.stringify(arr);
 
             // localStrageのsetItemで保存する
             localStorage.setItem('memoApp', arrStr);
+
+            // htmlの機能をなくす命令
+            return false;
         }     
         
     });
@@ -84,7 +86,27 @@ $(function(){
     // ラジオボタンが押されたら円の背景をグレーにし、テキストに打ち消し線をつける
     $('.task-list').on('click','li .btn',function(){
         $(this).toggleClass('checked');
-        $(this).parent().find('.title').toggleClass('checked');
+        $(this).parent().find('.title').toggleClass('checked'); 
+               
+        // ラジオボタンが押されたliの番号を取得する
+        // 番号を取得したいときはその親liにする checkedのときはthis
+        var num = $(this).parent().index();
+        console.log(num);
+        
+        // ラジオボタンのクラスにcheckedがあるかどうか取得する(あるかどうかはis〜)
+        var isChecked= $(this).hasClass('checked');
+        console.log(isChecked);
+        
+        // numの番号の配列に保存
+        // arr　→　ローカルストレージに保存（ボタンが押された時にもうLSに保存）
+        arr[num].check = isChecked;
+        
+        // localStorageに保存
+        var arrStr =JSON.stringify(arr);
+
+        // localStrageのsetItemで保存する
+        localStorage.setItem('memoApp', arrStr);
+
     });
 
     // ゴミ箱アイコンが押されたらtask-li li　を消す
@@ -95,11 +117,25 @@ $(function(){
             var btnTag = $(this).find('.btn');
             console.log(btnTag);
 
+            // リストの番号を取得
+            var num = $(this).index();
+            console.log(num);
+            
+
             if(btnTag.hasClass('checked')){
                 // checkedというクラスを持っていたらli（this）を消す。
                 $(this).remove();
+
+                // 配列から要素を削除　numから1つ消す
+                arr.splice(num,1);
             }   
         });
+
+            // localStrageに保存
+            var arrStr =JSON.stringify(arr);
+
+            // localStrageのsetItemで保存する
+            localStorage.setItem('memoApp', arrStr);
     });
 
     // .dateにメモ帳を開いた日付を表示させる
@@ -157,11 +193,16 @@ function showData(data){
         var imgTag = '<img src="images/icon_rain.svg">';
         $('.weather_icon').append(imgTag);
 
-    } else {
+    } else if(main == 'Snow') {
         // 雪
         $('.bg').addClass('bg_gra_snow');
         // オリジナルアイコンを表示
         var imgTag = '<img src="images/icon_snow.svg">';
+        $('.weather_icon').append(imgTag);
+    } else {
+        $('.bg').addClass('bg_gra_ghost');
+        // オリジナルアイコンを表示
+        var imgTag = '<img src="images/icon_ghost.svg">';
         $('.weather_icon').append(imgTag);
     }
 
@@ -181,16 +222,15 @@ function showData(data){
     $('.temp').append(tempData+'℃');
 }
 
-
-
 // 関数の呼び出し　ajax通信が失敗した場合
 function showError(){
     console.log('失敗しました');
 }
 
 // リストを表示する
+// LSから配列に持ってくる
 function loadList(){
-      // ロードされた時にリストを再現する
+    // ロードされた時にリストを再現する
     // 1. localStrageから値をロードする
     var arrStr = localStorage.getItem('memoApp');
 
@@ -202,14 +242,31 @@ function loadList(){
     // 3. localStrageの値をarrに保存する
     arr = JSON.parse(arrStr);
 
-    // 4. arrの値をliに表示する
+    // 4. arrの値をliに表示する（見た目）
     for(var i=0; i<arr.length; i++){
         var titleStr = arr[i].title; 
-        var liTag = '<li>'+'<span class="btn"></span>'+
-        '<span class="title" contentEditable="true">'+titleStr+'</span>'+
-    '</li>';
-        
+
+        // trueだったらcheckedを入れる
+        // flseだったらcheckedを入れない　のif文
+
+        // trueかfalseどちらかが入る場合はis〜を使う
+        var isChecked = 　arr[i].check;
+        console.log(isChecked);
+
+        if(isChecked ===true){
+            // チェックを入れる
+            var liTag = '<li>'+'<span class="btn checked"></span>'+
+            '<span class="title checked" contentEditable="true">'+titleStr+'</span>'+
+        '</li>';
+        }else{
+            // チェック入れない
+            var liTag = '<li>'+'<span class="btn"></span>'+
+            '<span class="title" contentEditable="true">'+titleStr+'</span>'+
+        '</li>';
+        }
+  
         // .task-listに追加
         $('.task-list').append(liTag);
     }
 }
+
